537d66db9a65cb88a3134a577fe115bcb718fa72
nitinkashyap488 <nitinkashyap488@gmail.com>
2024-03-07 22:28:27 +0530

    base URL change

--- src/Config/BaseUrl.js
+++ src/Config/BaseUrl.js
@@ -2,4 +2,5 @@
 // export const API_BASE_URL = "http://localhost:5454";
 
 // server URL
-export const API_BASE_URL = "";
+export const API_BASE_URL =
+  "https://socialmediaserver-production-5852.up.railway.app";
